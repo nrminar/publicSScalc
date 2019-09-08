3b51f6e18725ccf4b26d4bf261f1897766f08d44
Nolan Minar <nolanminar@Nolans-MacBook-Air.local>
2019-09-08 14:25:05 -0500

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-app.use(express.static('public'));
+app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.listen(PORT, () => {
     console.log(`PORT: ${PORT}`);
